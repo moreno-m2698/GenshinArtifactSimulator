af71c0df8b24e73deccc9609344273be8c17d7f4
moreno-m2698 <moreno.mw2698@gmail.com>
2024-05-13 15:14:10 -0400

    new commit

---
+++ src/components/Inventory.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Artifact from '../enums/types.ts'
+
+const inventory: Artifact[] = ref([])
+</script>
---
+++ src/views/EquipmentView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue'
+import ArtifactRoller from '../components/ArtifactRoller.vue';
+</script>
+
+<template>
+  <main>
+    <h1>Equipment View</h1>
+    <ArtifactRoller />
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
