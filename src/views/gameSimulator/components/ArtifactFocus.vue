5f1e22d9fd1424ce27d6ced93173ab5d4996ea98
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-01 09:57:56 -0400

    ArtifactFocus.vue: fixed import

--- src/views/gameSimulator/components/ArtifactFocus.vue
+++ src/views/gameSimulator/components/ArtifactFocus.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { Artifact } from '../utilities/types/types';
+import type { Artifact } from '../../../utilities/types/artifactTypes';
 
 const props = defineProps<{
   artifact: Artifact;
